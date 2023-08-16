b56fb8d571f78fe5e1a51b4b2c61dbfb610ad294
prashant-naik-au52 <psprashantanilgod@gmail.com>
2023-08-16 16:51:18 +0530

    Back

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const colors = require('colors')
-const dotenv = require('dotenv')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const colors = require('colors');
+const dotenv = require('dotenv');
+const connectDb = require('./config/db');
 
 dotenv.config()
+ 
+connectDb();
 
 const app = express()
 
